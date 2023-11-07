c16027736d23107087c8e88878906fe8b4c1ac4b
Lexi Harrop <lharrop@lcs.on.ca>
2023-11-07 14:31:33 -0500

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Lexi on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
